3389076ac9e122c4b6908a5664bde922cc6249f2
Asharulislam <immaster35@gmail.com>
2020-11-02 21:22:19 +0500

    first

---
+++ lib/main.dart
@@ -0,0 +1,196 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:painting/color_boxes.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Material App',
+      home: Scaffold(
+        body: MainView(),
+      ),
+    );
+  }
+}
+
+class MainView extends StatefulWidget {
+  @override
+  _MainViewState createState() => _MainViewState();
+}
+
+class _MainViewState extends State<MainView> {
+  List<Offset> _offsets = [];
+  Color activeColor;
+  List<Color> _colors = [];
+  List<double> _brushSizes = [];
+  double brushSize = 4;
+  void onChangeColor(Color value) {
+    activeColor = value;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onPanUpdate: (details) {
+        setState(() {
+          RenderBox _object = context.findRenderObject();
+          Offset _locationPoints =
+              _object.globalToLocal(details.globalPosition);
+
+          _offsets.add(_locationPoints);
+          _colors.add(activeColor);
+          _brushSizes.add(brushSize);
+        });
+      },
+      onPanEnd: (details) {
+        _offsets.add(null);
+        _colors.add(null);
+        _brushSizes.add(null);
+      },
+      child: Stack(
+        children: <Widget>[
+          Center(
+            child: Image.asset("images/Maths.png"),
+          ),
+          CustomPaint(
+            painter: MyPainter(
+                offsets: _offsets, colors: _colors, brushSizes: _brushSizes),
+            size: Size.infinite,
+          ),
+          SafeArea(
+            top: true,
+            child: new ListTile(
+              title: new ColorBoxGroup(
+                width: 25.0,
+                height: 25.0,
+                spacing: 10.0,
+                colors: [
+                  Colors.blue,
+                  Colors.red,
+                ],
+                groupValue: activeColor,
+                onTap: (color) {
+                  setState(() {
+                    onChangeColor(color);
+                  });
+                },
+              ),
+            ),
+          ),
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                new CircleButton(
+                  onTap: () {
+                    setState(() {
+                      brushSize = 4;
+                    });
+                  },
+                  iconData: Icons.brush,
+                  iconSize: 30,
+                ),
+                new CircleButton(
+                  onTap: () {
+                    setState(() {
+                      brushSize = 7;
+                    });
+                  },
+                  iconData: Icons.brush,
+                  iconSize: 40,
+                ),
+                new CircleButton(
+                  onTap: () {
+                    setState(() {
+                      brushSize = 10;
+                    });
+                  },
+                  iconData: Icons.brush,
+                  iconSize: 50,
+                ),
+                //Clear button
+                FloatingActionButton(
+                    child: Icon(Icons.clear),
+                    tooltip: "Erase",
+                    onPressed: () {
+                      setState(() {
+                       _offsets=[];
+                      });
+                    }),
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class MyPainter extends CustomPainter {
+  final List<Offset> offsets;
+  //final Color activeColor;
+  final List<Color> colors;
+  final List<double> brushSizes;
+  final brush = Paint()
+    ..strokeCap = StrokeCap.round
+    ..strokeWidth = 4.0
+    ..color = Colors.blue
+    ..isAntiAlias = true;
+
+  MyPainter(
+      {@required this.offsets,
+      @required this.colors,
+      @required this.brushSizes});
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    for (int i = 0; i < offsets.length - 1; i++) {
+      if (offsets[i] != null && offsets[i + 1] != null) {
+        brush.color = colors[i] == null ? Colors.blue : colors[i];
+        brush.strokeWidth = brushSizes[i];
+        canvas.drawLine(offsets[i], offsets[i + 1], brush);
+      }
+    }
+  }
+
+  @override
+  bool shouldRepaint(CustomPainter oldDelegate) {
+    return true;
+  }
+}
+
+class CircleButton extends StatelessWidget {
+  final GestureTapCallback onTap;
+  final IconData iconData;
+  final double iconSize;
+
+  const CircleButton({Key key, this.onTap, this.iconData, this.iconSize})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    double size = iconSize;
+
+    return new InkResponse(
+      onTap: onTap,
+      child: new Container(
+        width: size,
+        height: size,
+        decoration: new BoxDecoration(
+          color: Colors.white,
+          shape: BoxShape.circle,
+        ),
+        child: new Icon(
+          iconData,
+          color: Colors.black,
+          size: iconSize,
+        ),
+      ),
+    );
+  }
+}
